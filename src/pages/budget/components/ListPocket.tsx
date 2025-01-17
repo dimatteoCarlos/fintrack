ddc13fc8eed21fd3b9b1cdccd42e2cf8bea22b8e
CarlosDR <dimatteoca@gmail.com>
2025-01-17 14:17:06 -0400

    was refactored included default pocket list fake fetching data and styles dynamic status

--- src/pages/budget/components/ListPocket.tsx
+++ src/pages/budget/components/ListPocket.tsx
@@ -1,59 +1,112 @@
+//ListPocket.tsx
+
 import { StatusSquare } from '../../../general_components/boxComponents';
+import { DEFAULT_CURRENCY } from '../../../helpers/constants';
+import { currencyFormat } from '../../../helpers/functions';
+import { CurrencyType } from '../../../types/types';
+
+// import { useFetch } from '../../../hooks/useFetch.tsx';
+
+export type PocketsToRenderType = {
+  // name: 'Name',
+  //     description: 'Description',
+  //     saved: 'Saved',
+  //     goal: '/Goal',
+
+  pocketName: string;
+  description: string;
+  saved: number;
+  goal: number;
+  currency?: CurrencyType;
+  status?: number;
+};
+
+const defaultPocketList: PocketsToRenderType[] = [
+  {
+    pocketName: 'Name Pocket 01',
+    description: 'Description 01',
+    saved: Math.random() * 100,
+    goal: Math.random() * 100,
+    status: Math.floor((Math.random() - 0.5) * 100),
+  },
+  {
+    pocketName: 'Name Pocket 02',
+    description: 'Description 02',
+    saved: 99,
+    goal: 100,
+    currency: 'cop',
+  },
+  {
+    pocketName: 'Name Pocket 03',
+    description: 'Description 03',
+    saved: Math.random() * 100,
+    goal: Math.random() * 100,
+  },
+  {
+    pocketName: 'Name Pocket 04',
+    description: 'Description 04',
+    saved: Math.random() * 100,
+    goal: Math.random() * 100,
+    currency: 'eur',
+  },
+];
 
 function ListPocket() {
-  const pockets = [
-    {
-      name: 'Name',
-      description: 'Description',
-      saved: 'Saved',
-      goal: '/Goal',
-    },
-    {
-      name: 'Name',
-      description: 'Description',
-      saved: 'Saved',
-      goal: '/Goal',
-    },
-    {
-      name: 'Name',
-      description: 'Description',
-      saved: 'Saved',
-      goal: '/Goal',
-    },
-    {
-      name: 'Name',
-      description: 'Description',
-      saved: 'Saved',
-      goal: '/Goal',
-    },
-  ];
+  //List Pocket
+  //DATA FETCHING
+  //const{ data, isLoading, error } = useFetch<PokcketsType>(url_budget_pocket);//Data Fetching //Este endpoint no existe
+  //temporary values
+  let data: PocketsToRenderType[] = [],
+    isLoading = false,
+    error = null;
+
+  const pocketList: PocketsToRenderType[] =
+    !isLoading && !error && data?.length
+      ? data.map(({ pocketName, description, saved, goal, currency }) => ({
+          pocketName,
+          description,
+          saved,
+          goal,
+          currency,
+        }))
+      : defaultPocketList;
+
+  // en el backend: generar la data segun estructura de los datos a renderizar, es decir,
+  //agrupar para cada pocket el saved y el goal, de cada uno sde los movimientos, la sumatoria de saved se refleja en el saved, ,  y el status seria el resultado de la resta entre el budget - expense de cada categoria, o si se prefiere reflejar el status de una vez.
+  //no se esta claro, si los valores o informacion se obtendra de los movimientos de expense realizados en cada categoria.
 
   return (
     <article className='list__main__container'>
-      {pockets.map((pocket, indx) => {
-        const { name, description, saved, goal } = pocket;
+      {pocketList.map((pocket, indx) => {
+        const { pocketName, description, saved, goal, currency } = pocket;
         return (
           <div
             className='card__tile__pocket line__container '
             key={`pockect-${indx}`}
           >
-            {/* <CardTilePocket key={`pockect-${indx}`}> */}
             {/* <PocketLeftTile> */}
             <div className='tile__left'>
-              <div className='tile__title'>{name}</div>
+              <div className='tile__title'>{pocketName}</div>
               <div className='tile__subtitle'>{description}</div>
             </div>
-            {/* </PocketLeftTile> */}
 
             {/* <PocketRightTile> */}
             <div className='tile__right'>
-              <div className='tile__title'>{saved}</div>
+              <div className='tile__title'>
+                saved:{' '}
+                {currencyFormat(currency ?? DEFAULT_CURRENCY, saved, 'en-US')}
+              </div>
               <div className='tile__subtitle flx-row-sb'>
-                {goal} <StatusSquare />
+                <span className='tile__subtitle tile__subtitle--opc'>
+                  goal:{' '}
+                  {currencyFormat(currency ?? DEFAULT_CURRENCY, goal, 'en-US')}{' '}
+                  &nbsp;
+                </span>
+
+                {/* {'definir regla de negocio, creo que lo ideal es que no haya diferencia entre los montos saved y goal'} */}
+                <StatusSquare alert={saved - goal <= 0 ? 'alert' : ''} />
               </div>
             </div>
-            {/* </PocketRightTile> */}
-            {/* </CardTilePocket> */}
           </div>
         );
       })}
