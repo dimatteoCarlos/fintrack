bfd79c771de1832fb7f851163220fc46e1b49a14
CarlosDR <dimatteoca@gmail.com>
2025-01-17 14:14:50 -0400

    minimum changes

--- src/pages/budget/components/ListCategory.tsx
+++ src/pages/budget/components/ListCategory.tsx
@@ -3,26 +3,26 @@ import {
   StatusSquare,
 } from '../../../general_components/boxComponents.tsx';
 import { currencyFormat } from '../../../helpers/functions.ts';
-import { CurrencyType } from '../../../types/types.ts';
-import { useFetch } from '../../../hooks/useFetch.tsx';
+import { CurrencyType, StatusType } from '../../../types/types.ts';
+// import { useFetch } from '../../../hooks/useFetch.tsx';
 
-export type CategoriesToRender = {
+export type CategoriesToRenderType = {
   categoryName: string;
   spent: number;
   status: number;
   budget: number;
   currency?: CurrencyType;
+  category_id?: number;
 };
 
-export type StatusType = 'positivo' | 'negativo';
-
-const defaultCategoryBudget: CategoriesToRender[] = [
+const defaultCategoryBudget: CategoriesToRenderType[] = [
   {
     categoryName: 'Category Name 1',
     spent: Math.random() * 100,
     status: Math.floor((Math.random() - 0.5) * 100),
     budget: Math.random() * 100,
     currency: 'usd',
+    category_id: Math.round(Math.floor(Math.random() * 100)),
   },
   {
     categoryName: 'Category Name 2',
@@ -30,6 +30,7 @@ const defaultCategoryBudget: CategoriesToRender[] = [
     status: Math.floor((Math.random() - 0.5) * 100),
     budget: Math.random() * 100,
     currency: 'cop',
+    category_id: Math.round(Math.floor(Math.random() * 100)),
   },
   {
     categoryName: 'Category Name 3',
@@ -37,6 +38,7 @@ const defaultCategoryBudget: CategoriesToRender[] = [
     status: Math.floor((Math.random() - 0.5) * 100),
     budget: Math.random() * 100,
     currency: 'eur',
+    category_id: Math.round(Math.floor(Math.random() * 100)),
   },
   {
     categoryName: 'Category Name 4',
@@ -44,37 +46,23 @@ const defaultCategoryBudget: CategoriesToRender[] = [
     status: Math.floor((Math.random() - 0.5) * 100),
     budget: Math.random() * 100,
     currency: 'cop',
+    category_id: Math.round(Math.floor(Math.random() * 100)),
   },
-
-  // {
-  //   categoryName: 'Category Name',
-  //   spent: 'spent',
-  //   statusTitle: 'status',
-  //   budget: 'budget',
-  // },
 ];
-
-const statusFn = (budget: number, spent: number): StatusType => {
-  const diff = budget - spent;
-  // const type = diff > 0 ? 'debtor' : diff < 0 ? 'lender' : 'none';
-  const type = diff >= 0 ? 'positivo' : 'negativo'; // o puede ser los colores de una vez green / red, etc.
-
-  return type;
-};
 
 function ListCategory() {
   //List Category
   //DATA FETCHING
   //const{ data, isLoading, error } = useFetch<BudgetsType>(url_categories_budget);//Data Fetching //Este endpoint no existe
-  let data: CategoriesToRender[] = [],
+  let data: CategoriesToRenderType[] = [],
     isLoading = false,
     error = null;
   // en el backend: generar la data segun estructura de los datos a renderizar, es decir,
   //agrupar para cada caategoria los expense y el budget, la sumatoria de los expense se refleja en el spent, y la sumatoria de los budget de c/cat seria el budget por categoria,  y el status seria el resultado de la resta entre el budget - expense de cada categoria, o si se prefiere reflejar el status de una vez.
   //no se esta claro, si los valores o informacion se obtendra de los movimientos de expense realizados en cada categoria.
 
-  const budgetList: CategoriesToRender[] =
-    data && !isLoading && !error && data.length
+  const budgetList: CategoriesToRenderType[] =
+    data && !isLoading && !error && data.length>0
       ? data.map((catBudget) => {
           const { categoryName, spent, status, budget } = catBudget;
 
@@ -90,7 +78,7 @@ function ListCategory() {
   //functions
   function onCategoryHandler(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     e.preventDefault();
-    
+
     console.log('budget and category movements', e.currentTarget); // creo que deberia incluir la fecha en el reporte
   }
 
@@ -100,18 +88,19 @@ function ListCategory() {
 
       <article className='list__main__container'>
         {budgetList.map((category, indx) => {
-          const { categoryName, spent, status, budget, currency } = category;
+          const { categoryName, spent, budget, currency } = category;
 
           return (
             <div className='box__container .flx-row-sb' key={indx}>
               <BoxRow>
-                <div id={categoryName}
+                <div
+                  id={categoryName} //or category_id taken from data base
                   className='box__title box__title--category__name '
-               
-                  onClick={(e)=>onCategoryHandler(e)}
+                  onClick={(e) => onCategoryHandler(e)}
                 >
                   {categoryName}{' '}
                 </div>
+
                 <div className='box__title--spent'>
                   spent: {currencyFormat(currency, spent, 'en-US')}{' '}
                 </div>
