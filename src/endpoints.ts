4925b94f6b260cd349420e436f941b36ab1036fc
CarlosDR <dimatteoca@gmail.com>
2025-01-19 17:13:03 -0400

    endpoint.ts updated

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -6,3 +6,4 @@ export const url_categories = BASE_URL + '/' + 'categories';
 export const url_investment_acc = BASE_URL + '/' + 'investment-accounts';
 export const url_investment = BASE_URL + '/' + 'investment';
 export const url_sources = BASE_URL + '/' + 'sources';
+export const url_budget = BASE_URL + '/' + 'budget';
