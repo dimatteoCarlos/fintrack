3ecc53748e48dc997201039ce77ed74b1c604b67
CarlosDR <dimatteoca@gmail.com>
2025-01-25 19:13:14 -0400

    expense include the Numeric Format value validation

--- src/pages/tracker/expense/Expense.tsx
+++ src/pages/tracker/expense/Expense.tsx
@@ -26,16 +26,18 @@ import {
 //-----temporarily 'till decide how to handle currencies
 const defaultCurrency = DEFAULT_CURRENCY;
 const formatNumberCountry = CURRENCY_OPTIONS[defaultCurrency];
-console.log('', { formatNumberCountry });
+// console.log('', { formatNumberCountry });
 
 // ********************PENDEINTE
 // INCLUIR VALIDACION POR NOTACION cientifica, hacer un componente input que incluya validacion en tiemo real, evaluar si mantener la validacion global al hacer submit, manejo de los mensajes de validacion.
 //verificar opciones inputmode para que aparezca solo teclado numerico, validacion con patter html, de una en el input
 
+//PROBAR formData para actualizar los valores de los inputs del formulario, mientras se evaluan como string, y se muestra el mensaje de validacion en tiempo real, paralelamente,  MANEJAR aparte los valores que se grabaran en el estado expense data, convirtiendolos a numero estandar, tal vez sea otra funcion de conversion a numero, mientra la que ya escribi sea para validar el estado de validacion, mientras se guarda el resultado, para actualizar el estado, que luego va al backend para guardarlo en bse de datos como numero, etc.
+
 //------------------------------------------------------
 //input expense data state variables
 type ExpenseDataType = {
-  amount: number | '';
+  amount: number;
   account: string;
   category: string;
   note: string;
@@ -43,11 +45,18 @@ type ExpenseDataType = {
 };
 
 const initialExpenseData: ExpenseDataType = {
-  amount: '',
+  amount: 0,
   account: '',
   category: '',
   note: '',
   currency: defaultCurrency,
+};
+
+// type FormNumberInputType = Omit<ExpenseDataType, 'amount'> & { amount: string };
+type FormNumberInputType = { amount: string };
+
+const initialFormData: FormNumberInputType = {
+  amount: '',
 };
 //------------------------------
 
@@ -97,90 +106,161 @@ function Expense() {
 
   //---states-------------
   const [currency, setCurrency] = useState<CurrencyType>(defaultCurrency);
-  const [expenseData, setExpenseData] = useState(initialExpenseData);
   const [isReset, setIsReset] = useState<boolean>(false);
-
   const [validationMessages, setValidationMessages] = useState<{
     [key: string]: string;
-  }>({});
+  }>(initialFormData);
 
-  //-----useEffect--------
-  // useEffect(() => {
-  //   updateDataCurrency(currency);
-  // }, [currency]);
+  const [expenseData, setExpenseData] = useState(initialExpenseData);
+
+  const [formData, setFormData] = useState(initialFormData);
 
   //----functions--------
   function updateDataCurrency(currency: CurrencyType) {
     setCurrency(currency);
     setExpenseData((prev) => ({ ...prev, currency: currency }));
   }
+  //-------
+  function checkNumberFormatValue(value: string): {
+    formatMessage: string;
+    valueNumber: string;
+    valueToSave: number;
+    isError: boolean;
+  } {
+    console.log('value:', value, typeof value);
+    const onlyDotDecimalSep = /^\d*(\.\d*)?$/g; //Normal numeric Format
+    const commaSepFormat = /^(\d{1,3})(,\d{3})*(\.\d*)?$/g;
+    const dotSepFormat = /^(\d{1,3})(\.\d{3})*(,\d*)?$/g;
+    const onlyCommaDecimalSep = /^\d*(\,\d*)$/g;
+    const notMatching = /([^0-9.,])/g;
 
-  // function checkNumberValue(value: number | string) {
-  //   console.log('value:', typeof value);
-  //   const result =
-  //     typeof value === 'number'
-  //       ? value
-  //       : value !== '' && !isNaN(parseFloat(value))
-  //       ? parseFloat(value.trim().replace(',', '.'))
-  //       : NaN;
-  //   if (isNaN(result)) {
-  //     setValidationMessages((prev) => ({
-  //       ...prev,
-  //       [validationMessages['amount']]: 'Please insert a valid number',
-  //     }));
-  //   }
+    //no matching character
+    if (notMatching.test(value)) {
+      return {
+        formatMessage: `not a valid number: ${value.match(notMatching)}`,
+        isError: true,
+        valueNumber: value.toString(),
+        valueToSave: 0,
+      };
+    }
+    //normal number
+    if (onlyDotDecimalSep.test(value)) {
+      const valueNumber = !isNaN(parseFloat(value)) ? parseFloat(value) : 0;
 
-  //   return result;
-  // }
-  function checkNumberValue(value: number | string) {
-    console.log('value:', typeof value);
-
-    // Si el valor es un número, lo devolvemos directamente
-    let result =
-      typeof value === 'number'
-        ? value
-        : value !== '' && !isNaN(parseFloat(value.replace(',', '.')))
-        ? parseFloat(value.trim().replace(',', '.')) // Normalizamos la coma a punto y parseamos
-        : NaN;
-
-    // Si el resultado es NaN, mostramos el mensaje de validación
-    if (isNaN(result)) {
-      setValidationMessages((prev) => ({
-        ...prev,
-        amount: 'Please insert a valid number',
-      }));
+      return {
+        formatMessage: 'normal numeric input', //'no separators with optional dot as decimal sep ',
+        valueNumber: valueNumber.toString(),
+        valueToSave: valueNumber,
+        isError: false,
+      };
     }
 
-    return result;
+    //only comma decimal
+
+    if (onlyCommaDecimalSep.test(value)) {
+      const valueNumber = !isNaN(parseFloat(value.replace(',', '.')))
+        ? parseFloat(value.replace(',', '.'))
+        : 0;
+
+      return {
+        formatMessage: ' comma as dec. sep.',
+        valueNumber: valueNumber.toString(),
+        valueToSave: valueNumber,
+        isError: false,
+      };
+    }
+
+    //comma separator, decimal dot
+    if (commaSepFormat.test(value)) {
+      const valueNumber = !isNaN(parseFloat(value.replace(/,/g, '')))
+        ? parseFloat(value.replace(/,/g, ''))
+        : 0;
+
+      return {
+        formatMessage: 'comma as th-sep , dot as dec-sep',
+        valueToSave: valueNumber,
+        valueNumber: value.toString(),
+        isError: false,
+      };
+    }
+
+    //dot as thousand separator, comma as decimal separator
+    if (dotSepFormat.test(value)) {
+      const valueNumber = !isNaN(
+        parseFloat(value.replace(/\./g, '').replace(',', '.'))
+      )
+        ? parseFloat(
+            parseFloat(value.replace(/\./g, '').replace(',', '.')).toFixed(2)
+          ) //fixed does not work
+        : 0;
+
+      return {
+        formatMessage: 'dot th-sep, comma dec-sep',
+        valueToSave: valueNumber,
+        valueNumber: value.toString(),
+        isError: false,
+      };
+    }
+
+    //----
+
+    return {
+      formatMessage: `Number format not valid`,
+      isError: true,
+      valueNumber: '',
+      valueToSave: 0,
+    };
   }
 
+  //===
   function updateTrackerData(
-    e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
-    // e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    // e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement> // with currentTarget
   ) {
-    // e.preventDefault();
-    const { name, value } = e.currentTarget;
-    // const { name, value } = e.target;
+    e.preventDefault();
+    const { name, value } = e.target;
 
-    const valueToSave =
-      name === 'amount' && !isNaN(checkNumberValue(value))
-        ? checkNumberValue(value)
-        : value;
+    //-----------
+    //the flag for number quantity type is amount in the name. need to fix it to put it in general receiven the numeric  name associated to the numeric input field to evaluate, it may be various field from formData
 
-    // const valueToSave =
-    //   name === 'amount' && !isNaN(parseFloat(value)) && value.trim() !== ''
-    //     ? value.includes(',')
-    //       ? parseFloat(value.replace(',', '.'))
-    //       : parseFloat(value)
-    //     : value;
+    if (name === 'amount') {
+      const { formatMessage, valueNumber, isError, valueToSave } =
+        checkNumberFormatValue(value);
 
-    setExpenseData((prev) => ({ ...prev, [name]: valueToSave }));
+      // Actualizar el estado numerico en el formulario
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+
+      console.log({ formatMessage, valueNumber, isError, valueToSave });
+
+      setValidationMessages((prev) => ({
+        ...prev,
+        [name]: ` * Format: ${formatMessage}`,
+      }));
+
+      if (isError) {
+        console.log('Number Format Error occurred');
+
+        setValidationMessages((prev) => ({
+          ...prev,
+          [name]: ` *Error: ${formatMessage}`,
+        }));
+      }
+
+      setExpenseData((prev) => ({ ...prev, [name]: valueToSave }));
+      return;
+    } else {
+      setExpenseData((prev) => ({ ...prev, [name]: value }));
+    }
   }
 
+  //---
   function onSaveHandler(e: React.MouseEvent<HTMLButtonElement>) {
     console.log('On Save Handler');
     e.preventDefault();
-    //temporarily 'till defining what format the numbers will be saved. Options: in ddbb as number, when showing as string with format
+
     const formattedNumber = numberFormat(expenseData.amount || 0);
     console.log(
       'Expense formatted amount as a string:',
@@ -191,7 +271,7 @@ function Expense() {
     //----------------------------------------------------------------------------------------
     //validation of entered data
     const newValidationMessages = validationData(expenseData);
-    console.log('validation mgs:', newValidationMessages);
+    // console.log('validation mgs:', newValidationMessages);
 
     if (Object.values(newValidationMessages).length > 0) {
       setValidationMessages(newValidationMessages);
@@ -207,6 +287,7 @@ function Expense() {
     setCurrency(defaultCurrency);
     setExpenseData(initialExpenseData);
     setValidationMessages({});
+    setFormData(initialFormData);
 
     setTimeout(() => setIsReset(false), 500);
   }
@@ -215,12 +296,21 @@ function Expense() {
 
   return (
     <>
-      <article className='expense' style={{ color: 'inherit' }}>
-        {/* start of top */}
+      <form className='expense' style={{ color: 'inherit' }}>
+        {/* start of card top */}
         <div className='state__card--top'>
           <div className='card--title'>
             Amount
-            <span className='validation__errMsg'>
+            <span
+              className='validation__errMsg'
+              style={{
+                color: `${
+                  validationMessages['amount']?.includes('Format')
+                    ? 'green'
+                    : 'red'
+                }`,
+              }}
+            >
               {validationMessages['amount']}
             </span>
           </div>
@@ -229,12 +319,10 @@ function Expense() {
             <input
               className='inputNumber'
               name='amount'
-              type='number'
-              step='any'
+              type='text'
               placeholder={`${trackerState}`}
-              value={expenseData?.amount}
-              onInput={updateTrackerData}
-              // onChange={updateTrackerData}
+              value={formData.amount} //only for numeric values
+              onChange={updateTrackerData}
             />
 
             <CurrencyBadge
@@ -251,6 +339,7 @@ function Expense() {
               {validationMessages['account']}
             </span>
           </div>
+
           <SelectComponent
             dropDownOptions={accountOptions}
             setSelectState={setExpenseData}
@@ -260,11 +349,11 @@ function Expense() {
             selectedValue={expenseData['account']}
           />
         </div>
-        {/* end of top */}
+        {/* end of card top */}
 
         <CardSeparator />
 
-        {/*start of bottom */}
+        {/*start of card bottom */}
         <div className='state__card--bottom'>
           <div className='card--title card--title--top'>
             Category{' '}
@@ -313,14 +402,7 @@ function Expense() {
 
           {/* end of bottom */}
         </div>
-
-        {/* <FormSubmitBtn
-          // btnTitle={'save'}
-          onClickHandler={onSaveHandler}
-        >
-          {'save'}
-        </FormSubmitBtn> */}
-      </article>
+      </form>
     </>
   );
 }
