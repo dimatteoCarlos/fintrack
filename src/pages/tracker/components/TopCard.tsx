354fae20e9a1e714b3af53361e79a03c0ab1a416
CarlosDR <dimatteoca@gmail.com>
2025-02-01 17:41:18 -0400

    TopCard component applied on tracker Expense

---
+++ src/pages/tracker/components/TopCard.tsx
@@ -0,0 +1,114 @@
+import CurrencyBadge from '../../../general_components/currencyBadge/CurrencyBadge';
+import { capitalize } from '../../../helpers/functions';
+import { CurrencyType } from '../../../types/types';
+import SelectComponent from './SelectComponent';
+
+type TopCardPropType = {
+  topCardElements: {
+    titles: { title1: string; title2: string };
+    value: string;
+
+    selectOptions: {
+      title: string;
+
+      options: {
+        value: string;
+        label: string;
+      }[];
+    };
+  };
+
+  validationMessages: { [key: string]: string };
+
+  updateTrackerData: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+
+  trackerName: string;
+
+  updateCurrency: (x: CurrencyType) => void;
+  currency: CurrencyType;
+  selectedValue: string;
+  setSelectState: React.Dispatch<React.SetStateAction<any>>; //check this later
+  isReset: boolean;
+  setIsReset: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const TopCard = ({
+  topCardElements,
+  validationMessages,
+  updateTrackerData,
+  trackerName,
+  updateCurrency,
+  currency,
+
+  setSelectState,
+  selectedValue,
+
+  isReset,
+  setIsReset,
+}: TopCardPropType): JSX.Element => {
+  const {
+    titles: { title1 },
+    titles: { title2 },
+    value,
+    selectOptions: topCardOptions,
+  } = topCardElements;
+
+  return (
+    <>
+      <div className='state__card--top  '>
+        <div className='card--title'>
+          {capitalize(title1)}
+          <span
+            className='validation__errMsg'
+            style={{
+              color: validationMessages[title1]?.includes('Format:')
+                ? 'green'
+                : 'red',
+            }}
+          >
+            {validationMessages[title1]}
+          </span>
+        </div>
+
+        <div className='card__screen'>
+          {/*       make input number a component       */}
+          <input
+            className='inputNumber'
+            name={title1}
+            type='text'
+            placeholder={trackerName}
+            value={value} //amountValue
+            onChange={updateTrackerData} //onAmountChange
+          />
+
+          <CurrencyBadge
+            variant={'tracker'}
+            updateOutsideCurrencyData={updateCurrency}
+            currency={currency}
+          />
+        </div>
+
+        <div className='card--title'>
+          {capitalize(title2)}
+          <span className='validation__errMsg'>
+            {' '}
+            {validationMessages[title2]}
+          </span>
+        </div>
+
+        <SelectComponent
+          dropDownOptions={topCardOptions}
+          isReset={isReset}
+          setIsReset={setIsReset}
+          optionKeySelected={title2}
+          setSelectState={setSelectState} //setSelectedAccount
+          selectedValue={selectedValue} //selectedAccount
+        />
+      </div>
+    </>
+  );
+};
+
+export default TopCard;
