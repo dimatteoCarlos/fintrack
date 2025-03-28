27a465bf3ad3c6a903ddb189db9a755a902043cc
CarlosDR <dimatteoca@gmail.com>
2025-03-28 13:52:38 -0400

    useFetch fixed fetchData into useEffect

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -11,37 +11,36 @@ export function useFetch<R>(url: string): FetchResponse<R> {
   const [data, setData] = useState<R | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null | string>(null);
-
-  const fetchData = async () => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response: AxiosResponse<R> = await axios.get(url);
-
-      if (response.status >= 200 && response.status < 300) {
-        const respData = (await response.data) as R;
-        setData(respData);
-      } else {
-        const errMsg = `Unexpected status code: ${response.status}`;
-        console.log(errMsg);
-        throw new Error(errMsg);
-      }
-    } catch (err: unknown) {
-      if (err) {
-        const safeError =
-          err instanceof Error ? err : new Error('Unexpected error');
-        console.error('error:', safeError.message);
-        setData(null);
-        setError(safeError);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
-    fetchData();
+    const fetchData = async (url: string) => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response: AxiosResponse<R> = await axios.get(url);
+
+        if (response.status >= 200 && response.status < 300) {
+          const respData = (await response.data) as R;
+          setData(respData);
+        } else {
+          const errMsg = `Unexpected status code: ${response.status}`;
+          console.log(errMsg);
+          throw new Error(errMsg);
+        }
+      } catch (err: unknown) {
+        if (err) {
+          const safeError =
+            err instanceof Error ? err : new Error('Unexpected error');
+          console.error('error:', safeError.message);
+          setData(null);
+          setError(safeError);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData(url);
 
     return () => {
       setIsLoading(false);
