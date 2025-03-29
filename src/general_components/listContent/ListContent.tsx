db7c75c377632d6f2b3862f120dec800bff39c34
CarlosDR <dimatteoca@gmail.com>
2025-03-29 10:59:47 -0400

    ListContent fixed with date.toLocaleString()

--- src/general_components/listContent/ListContent.tsx
+++ src/general_components/listContent/ListContent.tsx
@@ -32,7 +32,7 @@ function ListContent({ listOfItems }: ListContenPropType) {
                   </div>
                 </BoxRow>
 
-                {!!date && isDateValid(date) && (
+                {!!date && isDateValid(date.toLocaleString()) && (
                   <div className='box__subtitle'>
                     {`${new Intl.DateTimeFormat(
                       DATE_TIME_FORMAT_DEFAULT
